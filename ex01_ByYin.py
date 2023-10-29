ce6400085d25b91d57cd9553497d1ae6f3286b69
megadumpling411133 <boaz411133@gmail.com>
2023-10-30 01:11:23 +0800

    addd ByDavid

---
+++ ex01_ByYin.py
@@ -0,0 +1,36 @@
+
+# 改名  refactor > rename (Shift+F6)
+
+# 功能 也叫 函數 function
+# print() 顯示資料
+
+# 資料類型 Data Types
+#   整數 int
+#   浮點數 float 近似值 例如 0.1
+#   文字 str  用單雙引號包含
+
+# 運算
+#   + - * / %
+#   += -= *=
+
+print(100)
+print(0.1)
+print('Tom')
+
+x = 5
+x += 3
+print(x)
+x -= 3
+print(5)
+x *= 2
+print(x)
+
+名 = 'Tom'
+英 = 100
+數 = 99
+總 = 英 + 數
+平 = 總 / 2
+print(名, 英, 數, 總, 平)
+
+
+
