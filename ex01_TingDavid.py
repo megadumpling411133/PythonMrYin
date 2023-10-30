22725168fce829693db9633b876a3c45c0f884a9
megadumpling411133 <boaz411133@gmail.com>
2023-10-30 22:41:05 +0800

    20231030 upload 01TingDavid 02David 02Yin

---
+++ ex01_TingDavid.py
@@ -0,0 +1,37 @@
+# 改名  refactor > rename (Shift+F6)
+
+# 功能 也叫 函數 function
+# print() 顯示資料
+
+# 資料類型 Data Types
+#   整數 int
+#   浮點數 float 近似值 例如 0.1
+#   文字 str  用單雙引號包含
+
+# 運算
+#   + - * / %
+#   += -= *=
+
+print(100)
+print(0.1)
+print('Tom')
+
+x = 5
+x += 3
+print(x)
+x -= 3
+print(5)
+x *= 2
+print(x)
+
+名 = 'Tom'
+英 = 100
+數 = 99
+總 = 英 + 數
+平 = 總 / 2
+print(名, 英, 數, 總, 平)
+
+# 加入描述 使用 + 號 來串接字串 > 將 非字串轉為字串
+print("姓名: " + str(名),"英文分數: " +  str(英),"數學分數: " +  str(數),"總分: " +  str(總),"平均: " +  str(平))
+
+
